1e83aa7ed69b6c2db4f8a5852bfaabe29df7c3dc
RomanRHSelf <romanrh.self@gmail.com>
2024-04-13 16:16:43 +0300

    NameError: name BASE is not defined

--- MainApp/views.py
+++ MainApp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-items = [
+item_s = [
 {"id": 1, "name": "Кроссовки abibas" ,"quantity":5},
 {"id": 2, "name": "Куртка кожаная" ,"quantity":2},
 {"id": 5, "name": "Coca-cola 1 литр" ,"quantity":12},
@@ -11,11 +11,11 @@ items = [
 ]
 
 def home(request):
-    text = """
-    <h1>"Изучаем django"</h1>
-    <strong>Автор</strong>: <i>Иванов И.П.</i>
-    """
-    return HttpResponse(text)
+    #text = """
+    #<h1>"Изучаем django"</h1>
+    #<strong>Автор</strong>: <i>Иванов И.П.</i>
+    #"""
+    return render(request, "index.html") #HttpResponse(text)
 
 def about(request):
     text = """
@@ -30,14 +30,20 @@ def about(request):
 
 def item(request, item_id:int):
     text = None
-    for t in items:
+    for t in item_s:
         if t["id"] == item_id:
             #text = "item_id: " + str(item_id) + ", Наименование: " + str(t['name']) + ", Количество: " + str(t['quantity'])
             text = f"<h1>{item_id=}</h1> \
                      <strong>Наименование</strong>: <i>{t['name']}</i> <br> \
                      <strong>Количество</strong>: <i>{t['quantity']}</i>"
-    return HttpResponse(f"item_id: {str(item_id)} не найден" if text is None else text)
+            return HttpResponse(f"item_id: {str(item_id)} не найден" if text is None else text)
 
 def items(request):
-    text = "временная рыба"    
+    #<ol>
+    #   <li> ... </li>
+    #   <li> ... </li>
+    #   <li> ... </li>
+    #</ol>
+    text = "<h1> Список товаров </h1><ol>"    
+
     return HttpResponse(text)
